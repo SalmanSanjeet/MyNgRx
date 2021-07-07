2a60e123aaa0f5cb3b3dd3ccd109a665076439a7
Salman Sanjeetwala <salman405f@gmail.com>
2021-07-07 10:27:45 +0530

    component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/layout/dashboard/dashboard.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { YoutubeLayoutComponent } from './components/layout/youtube-layout/youtube-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HeaderComponent,
+    YoutubeLayoutComponent
   ],
   imports: [
     BrowserModule
